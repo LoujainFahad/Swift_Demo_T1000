8c7b16ac47f716ebfb3826e8eb42ba10d98b6cac
loujain <loujain@MacBook-Air-loujain.local>
2021-10-11 13:03:00 +0300

    task2

---
+++ swift_demo_T1000/Week1/Day1/check_mark.swift
@@ -0,0 +1,17 @@
+//
+//  check_mark.swift
+//  swift_demo_T1000
+//
+//  Created by loujain on 05/03/1443 AH.
+//
+
+import Foundation
+
+//print("Enter your Mark :" , terminator: "")
+//let Mark: Double = Utils.readDouble()
+//
+//print("Enter your Full Mark :" , terminator: "")
+//let  fullMark : Int = Utils.readInt()
+//
+//let pct = Mark / Double (fullMark) * 100
+//print ("The prcentage :" , round (pct * 100) / 100 ,"%")
--- swift_demo_T1000/main.swift
+++ swift_demo_T1000/main.swift
@@ -8,11 +8,25 @@
 import Foundation
 
 
-print("Enter your Mark  :")
-var Mark: Double = Utils.readDouble()
+print("Enter your Mark :" , terminator: "")
+let Mark: Double = Utils.readDouble()
 
-print("Enter your Full Mark  :")
-var  fullMark : Int = Utils.readInt()
+print("Enter your Full Mark :" , terminator: "")
+let  fullMark : Int = Utils.readInt()
 
-var pct = Mark / Double (fullMark) * 100
-print ("Round : " , round (pct*100))
+let pct = Mark / Double (fullMark) * 100
+print ("The prcentage :" , round (pct * 100) / 100 ,"%")
+
+//________
+
+print("Enter your weight :" , terminator: "")
+let weight: Double = Utils.readDouble()
+//
+print("Enter your height :" , terminator: "")
+let  height : Double = Utils.readDouble()
+
+let h = (height / 100)
+
+let bmi = weight / pow(h , 2)
+
+print ("the BMI" , round (bmi * 100) / 100, "%")
